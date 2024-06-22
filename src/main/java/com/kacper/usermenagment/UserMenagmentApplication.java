aa0cf53aa6088c8b49069372619e711c76583544
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-06-22 20:48:02 +0200

    initial commit

---
+++ src/main/java/com/kacper/usermenagment/UserMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.kacper.usermenagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMenagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMenagmentApplication.class, args);
+    }
+
+}
