913ecc63cc291e4680548f4c67ebe954739074c7
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-06-22 22:41:46 +0200

    added controller

---
+++ src/main/java/com/kacper/usermenagment/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.kacper.usermenagment.controller;
+
+import com.kacper.usermenagment.dto.ReqRes;
+import com.kacper.usermenagment.entity.OurUsers;
+import com.kacper.usermenagment.service.UserManagmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController
+{
+    @Autowired
+    private UserManagmentService userManagmentService;
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<ReqRes> register(@RequestBody ReqRes request) {
+        return ResponseEntity.ok(userManagmentService.register(request));
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<ReqRes> login(@RequestBody ReqRes request) {
+        return ResponseEntity.ok(userManagmentService.login(request));
+    }
+
+    @PostMapping("/auth/refresh")
+    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes request) {
+        return ResponseEntity.ok(userManagmentService.refreshToken(request));
+    }
+
+    @GetMapping("/admin/get-all-users")
+    public ResponseEntity<ReqRes> getAllUsers() {
+        return ResponseEntity.ok(userManagmentService.getAllUsers());
+    }
+
+    @GetMapping("/admin/get-user/{userId}")
+    public ResponseEntity<ReqRes> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.ok(userManagmentService.getUsersById(userId));
+    }
+
+    @GetMapping("/admin/update-user/{userId}")
+    public ResponseEntity<ReqRes> updateUser(@PathVariable Integer userId, @RequestBody OurUsers reqRes) {
+        return ResponseEntity.ok(userManagmentService.updateUser(userId, reqRes));
+    }
+
+    @GetMapping("/adminuser/get-profile")
+    public ResponseEntity<ReqRes> getProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        ReqRes response = userManagmentService.getMyInfo(email);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+}
